fc0579ffde3e13b43f2836ff47e7480ee7a35e3b
phanindra4568 <137140900+phanindra4568@users.noreply.github.com>
2025-12-02 13:54:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
-onst form = document.querySelector('form');
-const greeting = document.querySelector('#greeting');
+// Add your API endpoint here
+var API_ENDPOINT = "API_ENDPOIND_PASTE_HERE";
 
-form.addEventListener('submit', (event) => {
-	event.preventDefault();
-	const name = document.querySelector('#name').value;
-	greeting.textContent = `Hello, ${name}!`;
-});
-
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch(
-        "https://o73g5ptpujgtclinhzhhneplje0jogoh.lambda-url.us-east-1.on.aws/"
-    );
-    let data = await response.json();
-    counter.innerHTML = `Views: ${data}`;
+// AJAX POST request to save student data
+document.getElementById("savestudent").onclick = function(){
+    var inputData = {
+        "studentid": $('#studentid').val(),
+        "name": $('#name').val(),
+        "class": $('#class').val(),
+        "age": $('#age').val()
+    };
+    $.ajax({
+        url: API_ENDPOINT,
+        type: 'POST',
+        data:  JSON.stringify(inputData),
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+            document.getElementById("studentSaved").innerHTML = "Student Data Saved!";
+        },
+        error: function () {
+            alert("Error saving student data.");
+        }
+    });
 }
-updateCounter();
+
+// AJAX GET request to retrieve all students
+document.getElementById("getstudents").onclick = function(){  
+    $.ajax({
+        url: API_ENDPOINT,
+        type: 'GET',
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+            $('#studentTable tr').slice(1).remove();
+            jQuery.each(response, function(i, data) {          
+                $("#studentTable").append("<tr> \
+                    <td>" + data['studentid'] + "</td> \
+                    <td>" + data['name'] + "</td> \
+                    <td>" + data['class'] + "</td> \
+                    <td>" + data['age'] + "</td> \
+                    </tr>");
+            });
+        },
+        error: function () {
+            alert("Error retrieving student data.");
+        }
+    });
+}
