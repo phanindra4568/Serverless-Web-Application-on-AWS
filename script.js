de5d303d03285040b1bb1f3f86a49c9ccaffe4e2
phanindra4568 <137140900+phanindra4568@users.noreply.github.com>
2025-12-02 21:30:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Add your API endpoint here
-var API_ENDPOINT = "https://5kw5bhg59h.execute-api.us-east-2.amazonaws.com/prod";
+var API_ENDPOINT = "https://4ph3doa8bi.execute-api.us-east-2.amazonaws.com/prod";
 
 // AJAX POST request to save student data
 document.getElementById("savestudent").onclick = function(){
