f3f07a20524fb1d6aab374cbb7ac35084d69ea38
phanindra4568 <137140900+phanindra4568@users.noreply.github.com>
2025-12-02 23:08:16 +0530

    Update insertStudentData.py

--- insertStudentData.py
+++ insertStudentData.py
@@ -1,31 +1,23 @@
-import json
 import boto3
+import json
+import uuid
 
-# Create a DynamoDB object using the AWS SDK
-dynamodb = boto3.resource('dynamodb')
-# Use the DynamoDB object to select our table
-table = dynamodb.Table('studentData')
-
-# Define the handler function that the Lambda service will use as an entry point
 def lambda_handler(event, context):
-    # Extract values from the event object we got from the Lambda service and store in variables
-    student_id = event['studentid']
-    name = event['name']
-    student_class = event['class']
-    age = event['age']
+    body = json.loads(event["body"])
     
-    # Write student data to the DynamoDB table and save the response in a variable
-    response = table.put_item(
-        Item={
-            'studentid': student_id,
-            'name': name,
-            'class': student_class,
-            'age': age
-        }
-    )
-    
-    # Return a properly formatted JSON object
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('Students')
+
+    item = {
+        "studentId": str(uuid.uuid4()),
+        "name": body["name"],
+        "email": body["email"]
+    }
+
+    table.put_item(Item=item)
+
     return {
-        'statusCode': 200,
-        'body': json.dumps('Student data saved successfully!')
+        "statusCode": 200,
+        "headers": {"Access-Control-Allow-Origin": "*"},
+        "body": json.dumps({"message": "Student added successfully!"})
     }
