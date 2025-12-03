d61c61707274e6a6920a2977261d2f635bff7993
phanindra4568 <137140900+phanindra4568@users.noreply.github.com>
2025-12-03 20:15:31 +0530

    Update getStudents.py

--- getStudents.py
+++ getStudents.py
@@ -5,12 +5,11 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('Students')
 
 def lambda_handler(event, context):
-    response = table.scan()
+    response = table.scan()  # Get all items
+    students = response.get('Items', [])
 
     return {
         'statusCode': 200,
-        'headers': {
-            "Access-Control-Allow-Origin": "*"
-        },
-        'body': json.dumps(response['Items'])
+        'headers': {'Access-Control-Allow-Origin': '*'},
+        'body': json.dumps(students)
     }
